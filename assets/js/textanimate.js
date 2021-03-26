19c34179688d465c76b7c1b955e9d4a93dbe29a4
Rahul <thecoderrahul@gmail.com>
2021-03-26 19:28:04 +0530

    Prettified

---
+++ assets/js/textanimate.js
@@ -0,0 +1,23 @@
+var txtanimate = document.getElementById('textanimate');
+var typewriter = new Typewriter(txtanimate, {
+    loop: true
+});
+
+typewriter.pauseFor(2000)
+    .typeString('Hello')
+    .pauseFor(1200)
+    .deleteAll()
+    .typeString("I'am  <span id='animatetextcolour'>Rahul</span>")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Web Developer<span id="animatetextcolour">.</span>')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Graphic Designer<span id="animatetextcolour">.</span>')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Founder of <span id="animatetextcolour">zovo</span>')
+    .deleteAll()
+    .typeString('<span id="animatetextcolour">Welcome</span> to my site<span id="animatetextcolour">!</span>')
+    .pauseFor(1000)
+    .start();
--- assets/js/wave.js
+++ assets/js/wave.js
@@ -1,3 +1,4 @@
+
 class ShaderProgram {
 
     constructor( holder, options = {} ) {
