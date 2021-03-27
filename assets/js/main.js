d17bad85ea89117bfc79486eb1a0af10d1044069
Rahul <thecoderrahul@gmail.com>
2021-03-27 15:52:57 +0530

    parallax hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,14 +250,14 @@
   });
 
   /**
-   * Animation on scroll
+   * Animation on scroll AOS
    */
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
-      once: true,
-      mirror: false
+      once: false,
+      mirror: false,
     });
   });
 
